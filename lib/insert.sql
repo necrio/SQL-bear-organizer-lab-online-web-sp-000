257e53feee894b2327a774a0476b51565d921209
Keith Isham <kisham8899@gmail.com>
2019-06-18 16:57:33 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,2 +1,8 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 4, "asd","brown","nice", 0)
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 4, "asd","brown","nice", 0)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 4, "asd","brown","nice", 0)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 4, "asd","brown","nice", 0)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 4, "asd","brown","nice", 0)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 4, "asd","brown","nice", 0)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 4, "asd","brown","nice", 0)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 4, "asd","brown","nice", 0)
