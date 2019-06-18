bec71716afddaa20f7675cc8b638941d939754f6
Keith Isham <kisham8899@gmail.com>
2019-06-18 17:11:25 +0000

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,19 @@
+CREATE TABLE bears (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age int,
+  gender text,
+  color text,
+  temperament text,
+  alive int
+);
+
+-- CREATE TABLE bears (
+-- 	id INTEGER PRIMARY KEY,
+-- 	name TEXT,
+-- 	age INTEGER,
+-- 	gender TEXT,
+-- 	color TEXT,
+-- 	temperament TEXT,
+-- 	alive INTEGER
+-- ); 
---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 4, "asd","brown","nice", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 4, "asd","brown","nice", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 4, "asd","brown","nice", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 4, "asd","brown","nice", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 4, "asd","brown","nice", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 4, "asd","brown","nice", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 4, "asd","brown","nice", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 4, "asd","brown","nice", 0);
