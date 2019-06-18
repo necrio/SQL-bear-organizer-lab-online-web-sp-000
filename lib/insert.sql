01f99c7e7abb192ed9369748260094e9cf9ccb0d
Keith Isham <kisham8899@gmail.com>
2019-06-18 16:57:19 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,2 +1,2 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 4, "asd","brown","nice", 0)
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 4, "asd","brown","nice", 0)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 4, "asd","brown","nice", 0)
