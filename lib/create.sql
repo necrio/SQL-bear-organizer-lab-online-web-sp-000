91415c0834df2aacf7c6c7d982e07f1788df773c
Keith Isham <kisham8899@gmail.com>
2019-06-18 17:07:40 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,12 +1,9 @@
 CREATE TABLE bears (
-  ID INT PRIMARY KEY
-  name TEXT
-  age int
-  gender text
-  color text
-  temperament text
-  alive int
-
-
-
-);
+	id INTEGER PRIMARY KEY,
+	name TEXT,
+	age INTEGER,
+	gender TEXT,
+	color TEXT,
+	temperament TEXT,
+	alive INTEGER
+); 
